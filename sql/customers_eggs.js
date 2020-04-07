54c00783dad11385028c96fe56befe6a70bdb57d
Joachim <joachim.ekstroem@gmail.com>
2020-04-07 20:59:18 +0200

    Fixed bug in eggs.html with zero candies in egg

--- sql/customers_eggs.js
+++ sql/customers_eggs.js
@@ -82,8 +82,6 @@ async function getEggsSql(eggdata) {
     let data = []
     let candy = []
     let candydata = {}
-
-    console.log('Antal ägg i mongo: ' + eggdata.mongo.length)
    
 
         for(let i = 0; i < eggdata.mongo.length; i++) {
@@ -105,6 +103,7 @@ async function getEggsSqlQuery(eggdata, i, q) {
     let data = {}
  
     return new Promise((resolve, reject) => {
+        console.log(eggdata)
 
         if (eggdata[i][0].candy[q].candy_producers_id > 0) {
 
@@ -112,7 +111,8 @@ async function getEggsSqlQuery(eggdata, i, q) {
 
                     connection.query(`SELECT * FROM candy 
                                     JOIN candy_producers ON candy.id = candy_producers.candy_id
-                                    WHERE candy_producers.id = ` + eggdata[i][0].candy[q].candy_producers_id, (error, result, fields) => {
+                                    WHERE candy.id = ` + eggdata[i][0].candy[q].candy_producers_id, (error, result, fields) => {
+        
                     if (error) throw error
                     connection.release()
                         data.id = result[0].id
@@ -162,7 +162,7 @@ async function getMongoIds(id) {
 async function createNewEgg(eggInfo) {
  
     return new Promise((resolve, reject) => {
-        
+        console.log(eggInfo)
         eggInfo.newMongoId = String(eggInfo.newMongoId)
         pool((err, connection) => {
 
