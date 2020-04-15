8da17a68a7f22fb831a0ed936a9cf15a11570c04
Joachim <joachim.ekstroem@gmail.com>
2020-04-15 12:34:09 +0200

    Made changes in Auth.js and removed some commented code.

--- src/auth.js
+++ src/auth.js
@@ -5,7 +5,7 @@ const pool = require('../sql/connectionPool.js')
 
 
 
-async function loginCust(loginData){
+async function login(loginData, sqlQuery){
 
     
     let userDetails = {}
@@ -13,7 +13,7 @@ async function loginCust(loginData){
         
         pool((err, connection) => {
 
-            connection.query(`SELECT * FROM customers WHERE 1 `, (error, result, fields) => {
+            connection.query(sqlQuery, (error, result, fields) => {
                 
                 if (error) throw error
                 connection.release()
@@ -49,51 +49,11 @@ async function loginCust(loginData){
 
 }
 
-async function loginProd(loginData){
-    let userDetails = {}
-    return new Promise((resolve, reject) => {
-        
-        pool((err, connection) => {
-
-            connection.query(`SELECT * FROM producers WHERE 1 `, (error, result, fields) => {
-                
-                if (error) throw error
-                connection.release()
-
-                for ( let i = 0; i < result.length; i++) {
-                    
-
-                    if ( loginData.username === result[i].name && loginData.password === result[i].password){
-
-                        userDetails.id = result[i].id
-                        userDetails.name = result[i].name
-                        userDetails.password = result[i].password
-                        userDetails.loggedIn = true
-                        userDetails.message = "You are logged in =)"
-                        break
-
-                    }
-
-                }
-                if(userDetails.loggedIn){
-                    resolve(userDetails)
-                } else {
-                    userDetails.loggedIn = false
-                    userDetails.message = "Wrong username or password"
-                    reject(userDetails)
-                }
-                    
-
-            })
-          
-        })
-    })
-
-}
 
 async function loginCustomer(loginData, socket) {
     try {
-        let data = await loginCust(loginData)
+        const sqlQuery = `SELECT * FROM customers WHERE 1 `
+        let data = await login(loginData, sqlQuery)
         console.log(data)
 
         socket.emit('LoggedInAsCustomer', data)
@@ -105,7 +65,8 @@ async function loginCustomer(loginData, socket) {
 }
 async function loginProducer(loginData, socket) {
     try {
-        let data = await loginProd(loginData)
+        const sqlQuery = `SELECT * FROM producers WHERE 1 `
+        let data = await login(loginData, sqlQuery)
         console.log(data)
         socket.emit('LoggedInAsProducer', data)
     } catch (err) {
